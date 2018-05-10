97ab2c62b8513a33ba15bd9c95ab1efb3e9531b8
Kyle Glaws <krglaws@eckerd.edu>
2018-05-10 07:10:29 -0400

    Profile Activity Update

--- app/src/main/java/pelican/pelican/Profile.java
+++ app/src/main/java/pelican/pelican/Profile.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,6 +18,12 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        String userName = mAuth.getCurrentUser().getDisplayName();
+
+        TextView displayName = findViewById(R.id.userName);
+        displayName.setText(userName);
 
         Button signOut = findViewById(R.id.signOutButton);
         signOut.setOnClickListener(new View.OnClickListener() {
